8eb6c4383fb9c222adafee9d904519484101b473
AyeshaAyat <ayeshaayat184@gmail.com>
2025-10-12 23:53:32 +0600

    icon added

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import Container from "../Container";
 import Image from "../ui elements/Image";
 import logo from "/public/LOGO (2).png";
+import { FaBars } from "react-icons/fa";
+
 import Flex from "../Flex";
 const Navbar = () => {
   return (
     <>
       <Container>
-        <div className="main py-2   bg-yellow-300">
-          <Flex className="justify-between">
-            <div className="main  w-max ">
+        <div className="main py-2 px-20  bg-yellow-300">
+          <Flex className="justify-between ">
+            <div className="logo w-max ">
               <Image src={logo} alt={logo} className="h-[60px] " />
             </div>
-            <div className="icon">
-              
+            <div className="icon text-black  ">
+              <FaBars className="size-6" />
+
             </div>
           </Flex>
         </div>
