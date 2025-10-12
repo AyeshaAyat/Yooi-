0307e3c2cd5eca41d69ccb8e721342606acd14ad
AyeshaAyat <ayeshaayat184@gmail.com>
2025-10-13 02:28:01 +0600

    up part done

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import Container from "../Container";
+import Image from "../ui elements/Image";
+import bgFriends from "../../assets/bgFriends.png";
 const Login = () => {
   return (
-   <>
+    <>
+      <Container>
+        <div className="main py-2 text-center bg-[#e3e0e0] m-auto w-100 rounded-[6px] mt-6">
+          <div className="up flex px-3 justify-between scale-60 border-b-1">
+            <h1 className="text-[48px] text-[#111111] font-sans mt-4  ">---</h1>
+            <h1 className="text-[48px] text-[#111111] font-sans mt-4  ">
+              Yooi
+            </h1>
+            <Image src={bgFriends} alt={bgFriends} />
+          </div>
+          <div className="down">
+            
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
 
-  <div className="main py-5 bg-pink-400 "> its login</div>
-   
-   </>
-  )
-}
-
-export default Login
+export default Login;
